5e492eb3bf659592b9b45cd713322f7ce53fb4ff
deanm <dean@gmail.com>
2009-03-29 18:10:39 +0200

    Adds a demo of texturing support.
    A spinning cube with textured faces.  The texture is a picture of
    the top of a wood desk, taken on a Nikon D40X.

---
+++ demos/textured_cube.js
@@ -0,0 +1,75 @@
+// (c) Dean McNamee <dean@gmail.com>.  All rights reserved.
+
+function start3d(texture_image) {
+  var screen_canvas = document.getElementById('canvas');
+  var renderer = new Pre3d.Renderer(screen_canvas);
+
+  var cube = Pre3d.ShapeUtils.makeCube(1);
+
+  var w = texture_image.width;
+  var h = texture_image.height;
+
+  var texinfo = new Pre3d.TextureInfo();
+  texinfo.image = texture_image;
+  texinfo.u0 = 0;
+  texinfo.v0 = 0;
+  texinfo.u1 = 0;
+  texinfo.v1 = h;
+  texinfo.u2 = w;
+  texinfo.v2 = h;
+  texinfo.u3 = w;
+  texinfo.v3 = 0;
+
+  // Use the same texture and uv's for all of the faces.
+  renderer.texture = texinfo;
+  // We don't want to fill, it will show at the edges (and waste cpu).
+  renderer.fill_rgba = null;
+
+  var state = {
+    cube_rotate_y_rad: 0,
+    cube_rotate_x_rad: 0,
+    cube_x: 2,
+    cube_y: 0
+  };
+
+  function draw() {
+    renderer.transform.reset();
+    renderer.transform.rotateX(state.cube_rotate_x_rad);
+    renderer.transform.rotateY(state.cube_rotate_y_rad);
+    renderer.transform.translate(state.cube_x, state.cube_y, -4);
+    renderer.bufferShape(cube);
+    renderer.draw();
+    renderer.emptyBuffer();
+  }
+
+  renderer.camera.focal_length = 2.5;
+  DemoUtils.autoCamera(renderer, 0, 0, -8, 0, 0, 0, draw);
+
+  function spin_and_draw() {
+    state.cube_rotate_y_rad += 0.1;
+    state.cube_rotate_x_rad += 0.01;
+    state.cube_x = Math.sin(state.cube_rotate_y_rad / 2) * 3;
+    state.cube_y = Math.sin(state.cube_rotate_x_rad * 4) * 3;
+    draw();
+  };
+
+  var ticker = new DemoUtils.Ticker(30, spin_and_draw);
+
+  var toolbar = new DemoUtils.ToggleToolbar();
+  toolbar.addEntry('Go!', true, function(e) {
+    if (this.checked) {
+      ticker.start();
+    } else {
+      ticker.stop();
+    }
+  });
+  toolbar.populateDiv(document.getElementById('toolbar'));
+
+  ticker.start();
+}
+
+window.addEventListener('load', function() {
+  var img = new Image();
+  img.onload = function() { start3d(img); };
+  img.src = 'textured_cube_texture.jpg';
+}, false);
