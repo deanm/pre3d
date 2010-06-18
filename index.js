78470ca980860a593a9c875c43fd455556fa3dc4
Dean McNamee <dean@gmail.com>
2010-06-18 18:16:13 +0200

    Slightly redesign the pre3d page.  Add a link to GraphyCalc. Redirect visitors to loader.html that didn't have a demo js in the url.

--- index.js
+++ index.js
@@ -4,19 +4,25 @@ window.addEventListener('load', function() {
   var screen_canvas = document.getElementById('canvas');
   var renderer = new Pre3d.Renderer(screen_canvas);
 
-  var cube = Pre3d.ShapeUtils.makeCube(0.5);
+  var shape = Pre3d.ShapeUtils.makeOctahedron();
+  Pre3d.ShapeUtils.linearSubdivideTri(shape);
+  Pre3d.ShapeUtils.forEachVertex(shape, function(v, i, s) {
+    s.vertices[i] = Pre3d.Math.unitVector3d(v);  // TODO(deanm): inplace.
+  });
+  // We need to rebuild the normals after extruding the vertices.
+  Pre3d.ShapeUtils.rebuildMeta(shape);
   renderer.draw_overdraw = false;
   renderer.fill_rgba = null;
-  renderer.ctx.lineWidth = 1.5;
-  renderer.stroke_rgba = new Pre3d.RGBA(1, 0.7, 0, 0.3);
+  renderer.ctx.lineWidth = 0.9;
+  renderer.stroke_rgba = new Pre3d.RGBA(0x45/255, 0xb4/255, 0xef/255, 0.4);
 
   function setTransform(x, y) {
     var ct = renderer.camera.transform;
     ct.reset();
     ct.rotateZ(0.0);
-    ct.rotateY(2.06 * x - 0.5);
-    ct.rotateX(1.2 * y + 0.4);
-    ct.translate(0, 0, -10);
+    ct.rotateY(-2.06 * x - 0.5);
+    ct.rotateX(2.2 * y + 1.5);
+    ct.translate(0, 0, -12);
   }
 
   renderer.camera.focal_length = 11;
@@ -24,7 +30,7 @@ window.addEventListener('load', function() {
 
   function draw() {
     renderer.clearBackground();
-    renderer.bufferShape(cube);
+    renderer.bufferShape(shape);
     renderer.drawBuffer();
     renderer.emptyBuffer();
   }
